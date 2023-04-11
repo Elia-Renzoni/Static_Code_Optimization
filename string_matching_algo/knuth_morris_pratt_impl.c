9ca4df9d85125656bfe872386ea5dac77b48c440
Elia Renzoni <118525453+Elia-Renzoni@users.noreply.github.com>
2023-04-11 10:22:10 +0200

    Add files via upload

---
+++ string_matching_algo/knuth_morris_pratt_impl.c
@@ -0,0 +1,35 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *														   *
+ *												                   *
+ *	Author : Elia Renzoni											   *
+ *														   *
+ *	ANSI C implementation of a string matching algorithm - Knuth-Morris-Pratt String Matching Algorithm	   *
+ *														   *
+ *	Date : 11/04/2023											   *
+ *														   *
+ *														   *
+ *														   *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int  compute_prefixe_function(const int pattern[]);
+void kmp_matcher(int testo[], int pattern[]); 
+
+
+int main(void) {
+
+
+
+
+
+
+
+	return (0);
+}
+
+
