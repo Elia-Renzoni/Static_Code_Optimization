86121dcf3a1fe58dcd47703625e1329d29f13d50
Elia Renzoni <118525453+Elia-Renzoni@users.noreply.github.com>
2023-05-07 17:33:34 +0200

    Update rabin_karp_string_matching.s

--- asm_optimization/rabin_karp_string_matching.s
+++ asm_optimization/rabin_karp_string_matching.s
@@ -6,33 +6,36 @@
 
 
               .data
-testo:                   .string 'a', 'b', 'a', 'c', 'a', 'b', 'b', 'c'            ; testo 
-pattern:                 .string 'c', 'a', 'b'                                     ; pattern
-numero_caratteri_testo:  .word  8
-numero_carrateri_patt:   .word  3
-contatore_i:             .word  0
-contatore_j:             .word  0
-base_valori:             .word  8
-valore_massimo_testo:    .word  1
-p:                       .word  0
-t:                       .word  0
-numero_primo:            .word  13
-occorrenze:              .word  0
+testo:                   .string 'a', 'b', 'a', 'c', 'a', 'b', 'b', 'c', 'a', 'b', 'a', 'a'           ; testo 
+pattern:                 .string 'c', 'a', 'b'                                                        ; pattern
+dimensione_testo:        .word 12
+dimensione_pattern:      .word  3
+numero_primo             .word 13
+base_valori              .word 12                                                                     ; stesso valore della dimensione del testo
+
 
               .text
 start:
-    loop: 
-    
-    
-    
-    loop:
-    
-    
-    
-    loop:
-    
-    
+      L.S r1, testo(r0)                                       ; caricamento dei dati in memoria
+      L.S r2, pattern(r0)
+      L.W r3, dimensione_testo(r0)
+      L.W r4, dimensione_pattern(r0)
+      L.W r5, numero_primo(r0)
+      L.W r6, base_valori(r0)
+      DADDI r7, r0, testo                                     ; puntatore primo elemento dell'array - testo - 
+      DADDI r8, r0, pattern                                   ; puntatore primo elemento dell'array - pattern - 
+
+loop:
+
+
+
+loop:
+
+
+
+loop:
     
     
 end:
     HALT
+
