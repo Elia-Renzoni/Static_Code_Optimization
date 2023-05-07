38ff35310b094a70eb40a4177869f20ce01e554b
Elia Renzoni <118525453+Elia-Renzoni@users.noreply.github.com>
2023-05-07 15:00:57 +0200

    Update source.s

--- asm_optimization/source.s
+++ asm_optimization/source.s
@@ -1,17 +1,17 @@
 ; Implementazione in Assembly dell'algoritmo di String Matching Rabin - Karp
               .data
-testo:                  .char 'a', 'b', 'a', 'c', 'a', 'b', 'b', 'c'
-pattern:                .char 'c', 'a', 'b'
-numero_caratteri_testo  .word  8
-numero_carrateri_patt   .word  3
-contatore_i             .word  0
-contatore_j             .word  0
-base_valori             .word  8
-valore_massimo_testo    .word  1
-p                       .word  0
-t                       .word  0
-numero_primo            .word  13
-occorrenze              .word  0
+testo:                   .string 'a', 'b', 'a', 'c', 'a', 'b', 'b', 'c'
+pattern:                 .string 'c', 'a', 'b'
+numero_caratteri_testo:  .word  8
+numero_carrateri_patt:   .word  3
+contatore_i:             .word  0
+contatore_j:             .word  0
+base_valori:             .word  8
+valore_massimo_testo:    .word  1
+p:                       .word  0
+t:                       .word  0
+numero_primo:            .word  13
+occorrenze:              .word  0
 
               .text
 start:
