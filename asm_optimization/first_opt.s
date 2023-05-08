0e8a3b582dfb60b628258e67128b45125029c4d7
Elia Renzoni <118525453+Elia-Renzoni@users.noreply.github.com>
2023-05-08 14:43:16 +0200

    Create first_opt.s

---
+++ asm_optimization/first_opt.s
@@ -0,0 +1,25 @@
+; Prima Ottimizzazione Statica - Instruction Reordering
+; @authors Elia Renzoni, Alessio Visentino e Alessio Biagioli
+; @data 08/05/2023
+; @copyright 2023
+
+                .data
+                
+                
+                
+                
+                
+                .text
+start:
+
+
+
+
+
+
+
+
+
+
+end
+  HALT
