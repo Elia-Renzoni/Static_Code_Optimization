aab81aaf18d5f00996f095a65cac65c0ab58c6e5
Elia Renzoni <118525453+Elia-Renzoni@users.noreply.github.com>
2023-05-20 12:04:32 +0200

    Update ricerca_binaria_prima_implementazione.s

--- asm_optimization/ricerca_binaria_prima_implementazione.s
+++ asm_optimization/ricerca_binaria_prima_implementazione.s
@@ -3,16 +3,15 @@
 ; Progetto per la Sessione Estiva di Architettura degli Elaboratori
 ; @brief Implementazione in MIPS Assembly dell'algoritmo di ricerca dicotomica per array giá ordinati
 
-
                 .data    
 password: .word 12, 34, 56, 78, 123, 456, 678
 nuovo_codice_utente: .word 56
 indice_sx: .word 0
-indice_dx: .word 6
+indice_dx: .word 7
 indice_mx: .word 0
 esito_ricerca: .word 0
 
-              .text
+                .text
 start:
     lw r1, indice_sx(r0)
     lw r2, indice_dx(r0)
@@ -20,19 +19,20 @@ start:
     lw r4, esito_ricerca(r0)
     lw r5, nuovo_codice_utente(r0)
     daddi r5, r3, password
-func_ricerca_binaria:
 dadd r6, r1, r2                       ; inizializzazione del contatore indice_mx
 ddiv r3, r7, 2
 loop:
   lw r7, 0(r5)
-  controllo_spostamento:              ; imposto la condizione del ciclo annidato : indice_sx <= indice_dx &&  password[mx] != nuovo_codice_utente
-  bne r7, r5, controllo_espressione
-  controllo_espressione:
+  bne r1, r2, ricerca                 ; if r1 <= r2 then label
+  ricerca:
+  slt r8, r5, r7                      ; if nuovo_codice_utente < password[indice_mx]
+  bne r8, r0, spazio_ricerca_destra
+  beq r8, r0, spazio_ricerca_sinistra
   
   
   spazio_ricerca_destra:
   
-  spazione_ricerca_sinistra:
+  spazio_ricerca_sinistra:
   
   
 end:
