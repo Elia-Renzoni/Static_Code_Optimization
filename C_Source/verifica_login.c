c97a1557bbf43f48bc81c4941e9460d27187b659
Elia Renzoni <118525453+Elia-Renzoni@users.noreply.github.com>
2023-08-04 13:10:18 +0200

    Update verifica_login.c

--- C_Source/verifica_login.c
+++ C_Source/verifica_login.c
@@ -1,6 +1,7 @@
 
 int ricerca_password(int *password, int codice_utente, int numero_elementi) {
   int sx, dx, mx;
+  
   for (sx = 0, dx = numero_elementi - 1, mx = (sx + dx) / 2;
               ((sx <= dx) && (password[mx] != codice_utente));
        mx = (sx + dx) / 2) {
@@ -9,5 +10,6 @@ int ricerca_password(int *password, int codice_utente, int numero_elementi) {
         else
           sx = mx + 1;
   }
+  
   return ((sx <= dx)? mx : 1);
 }
